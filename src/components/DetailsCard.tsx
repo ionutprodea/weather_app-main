7926ba3cbd918117bbb1e3ae21371902a8468bff
Ionut Prodea <ionut.prodea@yahoo.com>
2024-06-05 15:18:08 +0300

    Added test details from api request to DetailsCard component

--- src/components/DetailsCard.tsx
+++ src/components/DetailsCard.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 const DetailsCard = ({ cityDetails }: Props) => {
   const [error, setError] = useState("");
-  const [details, setDetails] = useState(null);
+  const [details, setDetails] = useState<any>(null);
   useEffect(() => {
     const controller = new AbortController();
     axios
@@ -44,32 +44,34 @@ const DetailsCard = ({ cityDetails }: Props) => {
         <div className="d-flex justify-content-center mt-3">
           <h2>{LocationName(cityDetails.city)}</h2>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <div className="d-flex justify-content-between">
-              <p>Current temperature:</p>
-              <p>999°C</p>
-            </div>
-          </li>
-          <li className="list-group-item">
-            <div className="d-flex justify-content-between">
-              <p>Current temperature:</p>
-              <p>999°C</p>
-            </div>
-          </li>
-          <li className="list-group-item">
-            <div className="d-flex justify-content-between">
-              <p>Current temperature:</p>
-              <p>999°C</p>
-            </div>
-          </li>
-          <li className="list-group-item">
-            <div className="d-flex justify-content-between">
-              <p>Current temperature:</p>
-              <p>999°C</p>
-            </div>
-          </li>
-        </ul>
+        {details && (
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Current temperature:</p>
+                <p>{details.current.temperature_2m}°C</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Precipitation:</p>
+                <p>{details.current.precipitation} %</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Current temperature:</p>
+                <p>999°C</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Current temperature:</p>
+                <p>999°C</p>
+              </div>
+            </li>
+          </ul>
+        )}
       </div>
     </>
   );
