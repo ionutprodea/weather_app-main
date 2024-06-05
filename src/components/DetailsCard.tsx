0d32e55931c034d18ca2ad750d3adc27c927d1d3
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2024-06-05 19:43:18 +0300

    Updated details for current weather

--- src/components/DetailsCard.tsx
+++ src/components/DetailsCard.tsx
@@ -36,43 +36,71 @@ const DetailsCard = ({ cityDetails }: Props) => {
 
   return (
     <>
-      <div
-        className="container border rounded mt-5"
-        style={{ maxWidth: "800px" }}
-      >
-        {error && <p>{error}</p>}
-        <div className="d-flex justify-content-center mt-3">
-          <h2>{LocationName(cityDetails.city)}</h2>
-        </div>
-        {details && (
-          <ul className="list-group list-group-flush">
+      <h2 className="fw-normal mt-5">Current</h2>
+      {error && <p>{error}</p>}
+      {details && (
+        <div
+          className="container border rounded mt-3"
+          style={{ maxWidth: "700px" }}
+        >
+          <div className="d-flex justify-content-between mt-5">
+            <h3 className="fw-normal ms-3">{LocationName(cityDetails.city)}</h3>
+            <h3 className="fw-normal me-3">
+              {details.current.temperature_2m}°C
+            </h3>
+          </div>
+          <ul className="list-group list-group-flush mt-5">
             <li className="list-group-item">
               <div className="d-flex justify-content-between">
-                <p>Current temperature:</p>
-                <p>{details.current.temperature_2m}°C</p>
+                <p>Real Feel:</p>
+                <p>{details.current.apparent_temperature}° C</p>
               </div>
             </li>
             <li className="list-group-item">
               <div className="d-flex justify-content-between">
                 <p>Precipitation:</p>
-                <p>{details.current.precipitation} %</p>
+                <p>{details.current.precipitation}%</p>
               </div>
             </li>
             <li className="list-group-item">
               <div className="d-flex justify-content-between">
-                <p>Current temperature:</p>
-                <p>999°C</p>
+                <p>Humidity:</p>
+                <p>{details.current.relative_humidity_2m}%</p>
               </div>
             </li>
             <li className="list-group-item">
               <div className="d-flex justify-content-between">
-                <p>Current temperature:</p>
-                <p>999°C</p>
+                <p>Pressure:</p>
+                <p>{details.current.surface_pressure} mb</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Daily UV Index:</p>
+                <p>{Math.round(details.daily.uv_index_max[0])} max</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Wind:</p>
+                <p>{details.current.wind_speed_10m} km/h</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Wind Gusts:</p>
+                <p>{details.current.wind_gusts_10m} km/h</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="d-flex justify-content-between">
+                <p>Cloud Cover:</p>
+                <p>{details.current.cloud_cover}%</p>
               </div>
             </li>
           </ul>
-        )}
-      </div>
+        </div>
+      )}
     </>
   );
 };
