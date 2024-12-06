9ff0bfb36f9eb29197a56c036ce0ee96f2d3c95a
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2024-12-06 21:17:23 +0200

    Removed useEffect import, fixing Vercel deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { City } from "./components/City";
 import CitySearch from "./components/CitySearch";
 import DisplaySearchResults from "./components/DisplaySearchResults";
