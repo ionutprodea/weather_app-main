d055b283da2ff7eeed26ea0324197d312499fa12
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2025-01-08 00:08:37 +0200

    Added text, links and logos to about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import UserLocation from "./UserLocation";
+import geoapify_logo from "../images/geoapify_logo.png";
+import locationiq_logo from "../images/locationiq_logo.png";
+import openmeteo_logo from "../images/openmeteo_logo.png";
 
 const handelClick = () => {
   sessionStorage.setItem("toggleDetails", "");
@@ -17,32 +20,41 @@ const About = () => {
             className="container d-flex flex-column align-items-center mt-5"
             style={{ maxWidth: "620px" }}
           >
-            <h1 className="text-dark">About Skycast</h1>
-            <p className="mt-5 text ">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
-              atque consequuntur est, commodi voluptatum laboriosam voluptate
-              vel voluptatem accusantium rerum dignissimos aperiam recusandae
-              consectetur quibusdam, in ipsam officiis. Eius at soluta quod odio
-              laborum facere veritatis, nam earum totam. Inventore voluptate
-              dolorum, facilis quidem eum corporis omnis nihil voluptatibus
-              corrupti odio! Voluptatum, officia vero et fugit possimus, tempora
-              repellat numquam veritatis itaque omnis velit. Quae accusantium ea
-              at ratione rem ut, dolore libero blanditiis totam sint hic
-              aspernatur architecto natus eveniet error similique eaque porro id
-              eligendi? Consectetur, dolorum at ipsa modi numquam sequi, magni a
-              odio ducimus voluptatibus cum repellendus harum quod, nihil
-              expedita iusto ipsam. Quibusdam quasi ducimus, maiores, cumque
-              omnis odio aut iure ex ipsam alias ad, repellat rerum amet aperiam
-              aliquid corrupti modi reiciendis nesciunt commodi rem quod.
-              Deserunt repellat placeat nulla quos saepe sit eaque, consectetur
-              nemo nostrum distinctio amet in iste, a provident officia mollitia
-              cumque vel inventore quod. Facere, voluptates? Ut distinctio
-              nesciunt rerum saepe consequuntur sint reiciendis nisi corrupti
-              iste veniam sit exercitationem eos minus nihil odit, unde, commodi
-              magni possimus minima officiis nam a dolore? Quos aliquid unde
-              tempora, hic, porro obcaecati eum consequatur facilis
-              reprehenderit quaerat quia expedita nesciunt. Similique!
+            <h1 className="text-dark mt-5">About Skycast</h1>
+            <p className="mt-5 text mx-1">
+              Skycast was created as part of my web development portfolio. It
+              provides accurate meteorological data using Open-Meteo APIs and
+              location services provided by LocationIQ and Geoapify. Please note
+              that the APIs are on free usage plans, which means the app has a
+              limited number of queries each month. Once this limit is reached,
+              new queries cannot be processed until the counter resets.
+              Apologies for any inconvenience this may cause and appreciate your
+              understanding. If you like this app and would like to know more
+              about its creator,{" "}
+              <a href="https://ionutprodea.ro/" className="link" target="blank">
+                follow this link
+              </a>
+              .
             </p>
+            <h2 className="text-dark mt-5 mb-4">Created with: </h2>
+            <div
+              className="d-flex flex-row flex-wrap justify-content-evenly"
+              style={{ maxWidth: "620px" }}
+            >
+              <a
+                href="https://www.geoapify.com/"
+                target="blank"
+                className="m-3"
+              >
+                <img src={geoapify_logo} alt="geoapify logo" />
+              </a>
+              <a href="https://locationiq.com/" target="blank" className="m-3">
+                <img src={locationiq_logo} alt="locationiq logo" />
+              </a>
+              <a href="https://open-meteo.com/" className="m-3">
+                <img src={openmeteo_logo} alt="open meteo logo" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="d-flex flex-column align-items-center bg-dark py-2 mt-3">
